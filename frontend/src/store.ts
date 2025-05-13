42aa988c05db29afcf71dc0581faea6698492715
alex <nomfodm@gmail.com>
2025-05-14 00:26:44 +0300

    fix frontend: fully remove download redux reducer

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,7 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import AuthReducer from "./store/auth"
 import GameProfilesReducer from "./store/gameprofiles"
-import DownloadReducer from "./store/download"
 import GameReducer from "./store/game"
 import LauncherUpdateReducer from "./store/launcherUpdate"
 
@@ -9,7 +8,6 @@ export const store = configureStore({
     reducer: {
         auth: AuthReducer,
         gameProfiles: GameProfilesReducer,
-        download: DownloadReducer,
         game: GameReducer,
         launcherUpdate: LauncherUpdateReducer
     }
--- frontend/src/store/game.ts
+++ frontend/src/store/game.ts
@@ -1,5 +1,4 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {main} from "../wailsjs/go/models";
 
 export enum GameStatus {
     IDLE,
